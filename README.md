db39e4e1c62b1895b8d5874adcfcb4dc206a45c9
Muhammad Nouman <nomi9639@gmail.com>
2024-03-27 19:25:10 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## ERPNext HTML to PDF
+
+Convert Any Custom HTML to PDF
+
+#### License
+
+MIT
---
+++ erpnext_html_to_pdf/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "ERPNext HTML to PDF",
+			"type": "module",
+			"label": _("ERPNext HTML to PDF")
+		}
+	]
